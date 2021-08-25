7e9590fbe899c9c7fc2f3f1641a0e4bc887c6137
Kate <katiasolo@tut.by>
2021-08-25 11:02:38 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+const answers =[];
+answers[0] = prompt('Как ваше имя?','');
+answers[1] = prompt('Как ваша фамилия?','');
+answers[2] = prompt('Сколько лет?','');
+
+console.log(typeof(answers));
